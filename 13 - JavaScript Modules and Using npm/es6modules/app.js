2d252704324cd1e0d18b36fe0c8dbd59dea4e8b1
eneax <19818937+eneax@users.noreply.github.com>
2018-08-28 10:29:33 +0200

    Refactor es6modules

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -14,4 +14,6 @@ import User, { createURL, gravatar } from './src/user';
 const wes = new User('Wes Bos', 'wesbos@gmail.com', 'wesbos.com');
 const profile = createURL(wes.name);
 const image = gravatar(wes.email);
+console.log(wes);
+console.log(profile);
 console.log(image);
--- 13 - JavaScript Modules and Using npm/es6modules/src/user.js
+++ 13 - JavaScript Modules and Using npm/es6modules/src/user.js
@@ -2,14 +2,18 @@ import slug from 'slug';
 import { url } from './config';
 import md5 from 'md5';
 
+// Main function, so it's a default export 
 export default function User(name, email, website) {
   return { name, email, website };
 }
 
+// Named export
 export function createURL(name) {
+  // const url = 'http://wesbos.com';
   return `${url}/users/${slug(name)}`;
 }
 
+// Named export
 export function gravatar(email) {
   const hash = md5(email.toLowerCase());
   const photoURL = `https://www.gravatar.com/avatar/${hash}`;
